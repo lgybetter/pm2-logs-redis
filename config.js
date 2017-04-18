c9e02f6163782c1c6fd8be260a0fb14a3c7604ee
Jiangjie Yang <yangjiangjie@bbcloud.com>
2017-04-18 12:02:30 +0800

    init project
    Change-Id: I2f86f1341bc2cf530d217d5fa4529b8d2e19bbfc

---
+++ config.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = {
+	"redisKeyPrefix": "pm2logs",
+	"timeStampFormat": "",
+  "redisUrl": ""
+}
---
+++ index.js
@@ -0,0 +1,101 @@
+'use strict';
+const redis = require('redis');
+const Promise = require('bluebird');
+const moment = require('moment');
+
+Promise.promisifyAll(redis.RedisClient.prototype);
+Promise.promisifyAll(redis.Multi.prototype);
+
+const pm2 = require('pm2');
+const pmx = require('pmx');
+
+const os = require('os');
+let ips = [];
+var network = os.networkInterfaces();
+for(var dev in network){
+  for(var i = 0; i < network[dev].length; i++) {
+      var json = network[dev][i];
+      if(json.family == 'IPv4') {
+				ips.push(json.address);
+      }
+  }
+}
+let host = ips.sort().join(',');
+
+console.log('host', host);
+
+pmx.initModule({
+  widget: {
+    logo: 'http://semicomplete.com/presentations/logstash-monitorama-2013/images/elasticsearch.png',
+    theme: ['#141A1F', '#222222', '#3ff', '#3ff'],
+    el: {
+      probes: false,
+      actions: false
+    },
+    block: {
+      actions: false,
+      issues: false,
+      meta: false,
+    }
+  }
+}, (err, conf) => {
+  if (err) {
+      console.log('error', err);
+      return;
+  }
+	const config = require('./config') || {};
+	console.log(conf, config);
+
+
+	let timeStampFormat = config.timeStampFormat || "";
+	let redisKeyPrefix = config.redisKeyPrefix || "pm2logs";
+	let redisUrl = config.redisUrl || "";
+
+	let redisClient = redis.createClient(redisUrl);
+	redisClient.on("error", function(error) {
+    console.log(error);
+	});
+
+	const getRedisKeyName = () => {
+		if(timeStampFormat){
+			return redisKeyPrefix + moment().format(timeStampFormat);
+		}else{
+			return redisKeyPrefix;
+		}
+	}
+	const makeMsg = (packet, source) => {
+		return {
+			"@timestamp": new Date(packet.at),
+			"host": host,
+			"message": packet.data,
+			"pm2": {
+        "name": packet.process.name,
+				"source": source,
+				"pm_id": packet.process.pm_id,
+				"rev": packet.process.rev
+			}
+		};
+	}
+  pm2.connect(() => {
+    console.log('info', 'PM2: forwarding to redis');
+
+    pm2.launchBus((err, bus) => {
+      bus.on('log:PM2', function (packet) {
+				let msg = makeMsg(packet, 'stdout');
+        console.log('log:PM2', msg, JSON.stringify(msg), packet.data);
+				redisClient.rpushAsync(getRedisKeyName(), JSON.stringify(msg));
+      });
+      bus.on('log:out', function (packet) {
+				let msg = makeMsg(packet, 'stdout');
+        console.log('log:out', msg, JSON.stringify(msg), packet.data);
+				redisClient.rpushAsync(getRedisKeyName(), JSON.stringify(msg));
+      });
+      bus.on('log:err', function (packet) {
+				let msg = makeMsg(packet, 'stderr');
+        console.log('log:err', msg, JSON.stringify(msg), packet.data);
+				redisClient.rpushAsync(getRedisKeyName(), JSON.stringify(msg));
+      });
+    });
+  });
+
+});
